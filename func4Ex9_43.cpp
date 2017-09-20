089b28f9a798f1ea859ede6a7b24d140f8929ea6
laolian <lianjin@hnu.edu.cn>
2017-09-20 15:36:24 +0800

    Update and rename func4Ex9_43.cpp to 9_44.cpp

--- func4Ex9_43.cpp
+++
@@ -1,9 +0,0 @@
-bool isOver(const string::iterator &cur, const string &si, const string &OldVali) {
-	for (size_t cnt = 0; cnt != OldVali.size(); ++cnt) {
-		if (cur == si.end() - cnt)
-		{
-			return false;
-		}
-	}
-	return true;
-}
