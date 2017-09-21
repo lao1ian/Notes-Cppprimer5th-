10bd3c09af7ca5d9b6f3b8e5b1932d92815633af
laolian <lianjin@hnu.edu.cn>
2017-09-21 19:56:02 +0800

    Add files via upload

---
+++ ch10/ex10_21.cpp
@@ -0,0 +1,27 @@
+//1.ans是一个可调用对象，不是一个值。类似于函数？
+//2.为什么不用位置返回类型，不是说一个lambda体包含return意外以外的任何语句，编译器都会假定此lambda返回void吗？
+//Answer:貌似是在C++14 还是C++17中,取消了 只有一句话的限制。
+
+#include "stdafx.h"
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	int local_val = 7;
+	auto ans = [&local_val]()->bool{
+		if (local_val)
+		{
+			--local_val;
+			return false;
+		}
+		else return true;
+	};
+	while (!ans())
+	{
+		cout << local_val << endl;
+	}
+    return 0;
+}
+
