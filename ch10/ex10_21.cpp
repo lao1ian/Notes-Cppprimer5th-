d7ea823e9081d3cc0080e4d4c63dfbe553041baf
laolian <lianjin@hnu.edu.cn>
2017-09-21 18:04:56 +0800

    Update ex10_21.cpp

--- ch10/ex10_21.cpp
+++ ch10/ex10_21.cpp
@@ -1,6 +1,6 @@
 //1.ans是一个可调用对象，不是一个值。
 //2.为什么不用尾置返回类型，不是说一个lambda体包含return意外以外的任何语句，编译器都会假定此lambda返回void吗？
-//Answer：貌似是在C++14 还是C++17中,取消了 只有一句话的限制。
+//Answer:貌似是在C++14 还是C++17中,取消了 只有一句话的限制。
 
 #include "stdafx.h"
 #include<iostream>
